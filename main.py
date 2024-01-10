7be4a7448887159ae783502aa5fc9924d1419b54
vitalii-harhai <vitalii.harhai@gmail.com>
2024-01-10 21:18:30 +0200

    next commit

--- main.py
+++ main.py
@@ -27,19 +27,11 @@ def gender_event(human, events_for_woman, events_for_men):
 print(general_event(long_term_events.long_term_event))
 print(gender_event(test_human, small_events.events_for_woman, small_events.events_for_men))
 
-
-work = True
-
 unit_time = 0
 
 while True:
-    # читаем состояние персонажа
-    if work:
-        print('Отработал я, чем займусь')
-    else:
-        print('Чем займусь')
-    event = input('Введите занятие')
-    # обновляем состояние персонажа
+
+    test = input()
     unit_time += 1
     if unit_time < 48:
         break
