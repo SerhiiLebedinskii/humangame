d946080fb877249e5d02253574aef2bf35ef516b
vitalii-harhai <vitalii.harhai@gmail.com>
2024-01-10 19:57:20 +0200

    next commit

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ work = True
 unit_time = 0
 
 while True:
+    # читаем состояние персонажа
     if work:
         print('Отработал я, чем займусь')
     else:
         print('Чем займусь')
     event = input('Введите занятие')
+    # обновляем состояние персонажа
     if unit_time > 48:
         break
 
