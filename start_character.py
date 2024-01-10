0adc15832dd92deafdc8af2e60460c42cc930d60
SerhiiLebedinskii <144608984+SerhiiLebedinskii@users.noreply.github.com>
2024-01-10 20:35:14 +0200

    Add files via upload

---
+++ start_character.py
@@ -0,0 +1,11 @@
+import random
+def character()
+    gender_lst = ["male","female"]
+    mood_lst = ["normal","good","angry","happy","sad"]
+    name = ""
+    age = random.randint(10,81)
+    gender = random.choice(gender_lst)
+    balance = random.randint(1000,100000)
+    mood = random.choice(mood_lst)
+    time_balance = random.randint()
+    return 
