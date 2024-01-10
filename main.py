bc7d93ed09c69bfb4c479eedb0379c55830d0e54
vitalii-harhai <vitalii.harhai@gmail.com>
2024-01-10 19:55:39 +0200

    next commit

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ human = {
     'age': 25,
     'gender': 'male',
     'balance': 1000,
-    'mood': 'good',
+    'mood': 5,
     'time_balance': 300
 }
+work = True
+
+unit_time = 0
 
 while True:
-    pass
+    if work:
+        print('Отработал я, чем займусь')
+    else:
+        print('Чем займусь')
+    event = input('Введите занятие')
+    if unit_time > 48:
+        break
+
 
